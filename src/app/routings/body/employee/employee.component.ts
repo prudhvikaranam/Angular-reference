7951e4652e584206c7d8392835089564f14f3e74
PrudhviKaranam <prudhvikaranam537@gmail.com>
2021-06-24 13:38:47 +0530

    adding dummy commit

--- src/app/routings/body/employee/employee.component.ts
+++ src/app/routings/body/employee/employee.component.ts
@@ -11,8 +11,8 @@ export class EmployeeComponent implements OnInit {
   employee: {
     id: number,
     name: string,
-    allow: any,
-    admin : any
+    allow?: any,
+    admin?: any
  };
 
   constructor(private router: ActivatedRoute) { }
@@ -25,20 +25,21 @@ export class EmployeeComponent implements OnInit {
     // this.router.params.subscribe((currentparams) => {
     //   this.employee.id = currentparams['id'];
     //   this.employee.name = currentparams['name'];
-    // }); or below    
+    // }); 
+    // or below    
     
-    // this.router.params.subscribe((currentParams) => {
-    //   // this.employee = {
-    //   //   id : currentParams['id'],
-    //   //   name : currentParams['name']
-    //   // }
-    // })
+    this.router.params.subscribe((currentParams) => {
+      this.employee = {
+        id : currentParams['id'],
+        name : currentParams['name']
+      }
+    })
 
     // // console.log(this.router.snapshot.queryParams);
-    // this.router.queryParams.subscribe((queryparams) => {
-    //   this.employee.allow = queryparams['allowEdit'];
-    //   this.employee.admin = queryparams['isAdmin'];
-    // })
+    this.router.queryParams.subscribe((queryparams) => {
+      this.employee.allow = queryparams['allowEdit'];
+      this.employee.admin = queryparams['isAdmin'];
+    })
   }
 
 }
